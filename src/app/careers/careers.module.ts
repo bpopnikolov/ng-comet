eeb01ff5f38cb8a7b4a954e2968a92ec3e320944
Deyan Atanasov <deyan.atanasov@yahoo.com>
2018-04-30 12:38:27 +0300

    created the base structure for the Careers module

---
+++ src/app/careers/careers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CareersRoutingModule } from './careers-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CareersRoutingModule
+  ],
+  declarations: []
+})
+export class CareersModule { }
